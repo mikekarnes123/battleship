fe3bb29655027f5dfaf444abba266e9174fb31d9
Paul Kim <pykim0591@gmail.com>
2019-01-29 16:06:59 -0700

    Finish board up to valid placement

---
+++ test/board_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+
+class BoardTest < Minitest::Test
+  def setup
+    @board = Board.new
+
+  end
+
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
+
+  def test_it_has_cells_hash_4_by_4
+    cell_keys = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    assert_equal cell_keys ,@board.cells.keys
+  end
+
+  def test_it_can_tell_a_valid_coordinate
+    assert @board.valid_coordinate?("A1")
+    refute @board.valid_coordinate?("R1")
+  end
+
+  def test_if_valid_ship_placement
+    submarine = Ship.new("Submarine", 3)
+    cruiser = Ship.new("Cruiser", 2)
+
+    assert @board.valid_placement?(submarine, ["A2", "B2", "C2"])
+    refute @board.valid_placement?(submarine, ["A1", "A2", "A4"])
+    assert @board.valid_placement?(submarine, ["A1", "A2", "A3"])
+    refute @board.valid_placement?(cruiser, ["A1", "A2", "A3"])
+
+    assert @board.valid_placement?(submarine, ["B1", "B2", "B3"])
+
+
+  end
+end
