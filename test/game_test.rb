a6b83117c96f62dfa7a9d48f1714a7a91e99bd66
Paul Kim <pykim0591@gmail.com>
2019-01-30 10:11:38 -0700

    Create Game class/test

---
+++ test/game_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game'
+
+class GameTest < MiniTest::Test
+  def setup
+    @game = Game.new
+  end
+  
+  def test_it_exists
+    assert_instance_of Game, @game
+  end
+end
