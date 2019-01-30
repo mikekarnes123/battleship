770a32fc174433b8a3bceb3c96c112ed77269ea1
Paul Kim <pykim0591@gmail.com>
2019-01-30 13:18:14 -0700

    Add Game test

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,13 +1,35 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/game'
+require './lib/computer'
+require './lib/player'
+require_relative 'gets_chomp'
 
 class GameTest < MiniTest::Test
+  include GetsChomp
   def setup
     @game = Game.new
   end
   
   def test_it_exists
     assert_instance_of Game, @game
+  end
+
+  def test_menu
+    with_stdin do |player|
+      player.puts "q"
+      @game.menu
+      refute @game.ready
+      
+      player.puts "p"
+      @game.menu
+      assert @game.ready
+    end
+  end
+
+  def test_start_game_creates_player_and_computer
+    @game.start_game
+    assert_instance_of Computer, @game.computer
+    assert_instance_of Player, @game.player
   end
 end
