0dac272b9a8f13872e781c38875b1945b32389a3
sojurner <pykim0591@gmail.com>
2019-01-29 20:08:12 -0700

    Create valid_row method & remove literal string condtional

--- lib/board.rb
+++ lib/board.rb
@@ -9,8 +9,6 @@ class Board
       }
       cell_hash
     }
-    @letters = Array.new(4, ("A".."D").to_a)
-    @numbers = Array.new(4, (1..4).to_a)
     @occupied_cells = []
   end
 
@@ -25,17 +23,22 @@ class Board
       occupied = true if @occupied_cells.include?(coord)
     end
     return !occupied if occupied
-    valid_column = @letters.map.with_index {|letters_a, index|
-      letters_a.map { |letter|
-        "#{letter}#{@numbers[index][index]}"
-      }
-    }
-    valid_row = valid_column.transpose
-    [valid_row, valid_column].reduce(false) { |boolean, valids_array|
+    valid_columns = valid_rows.transpose
+    [valid_rows, valid_columns].reduce(false) { |boolean, valids_array|
       target_valid = valids_array.find { |valid_array| valid_array.include?(coordinates.first) }
       indices = coordinates.map { |coordinate| target_valid.index(coordinate) }
       boolean = indices.each_cons(2).all? { |a, b| b == a + 1 }
       return boolean if boolean
+    }
+  end
+
+  def valid_rows
+    letters = Array.new(4, ("A".."D").to_a)
+    numbers = Array.new(4, (1..4).to_a)
+    numbers.map.with_index { |numbers_a, index|
+      numbers_a.map { |number|
+        "#{letters[index][index]}#{number}"
+      }
     }
   end
 
@@ -47,22 +50,13 @@ class Board
   end
 
   def render(boolean=false)
-    if !boolean
-      "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
-    else
-     rows = @numbers.map.with_index { |numbers_a, index|
-        numbers_a.map { |number|
-          "#{@letters[index][index]}#{number}"
-        }
-      }
-      rendered_board = rows.map.with_index { |row_array, index|
+    rendered_board = valid_rows.map.with_index { |row_array, index|
       rendered_rows = row_array.map { |coordinate|
-        @occupied_cells.include?(coordinate) ? @cells[coordinate].render(true) : @cells[coordinate].render
+        @occupied_cells.include?(coordinate) && boolean ? @cells[coordinate].render(true) : @cells[coordinate].render
       }
       rendered_rows.unshift(("A".."D").to_a[index]) << "\n"
     }.join(" ")
-    "   1 2 3 4 \n #{rendered_board}"
-    end
+  "   1 2 3 4 \n #{rendered_board}"
   end
 
 
