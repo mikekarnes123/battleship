e23d9a118ce7ee9192baf56e7b2d1b12723c8e7d
sojurner <pykim0591@gmail.com>
2019-01-30 20:22:27 -0700

    Refactor code

--- lib/computer.rb
+++ lib/computer.rb
@@ -7,35 +7,22 @@ class Computer
   def initialize
     @board = Board.new
     @ships = [Ship.new("Cruiser", 2), Ship.new("Submarine", 3)]
-    @ships_coordinates = {}
+    @ships_coordinates = {"Cruiser" => [], "Submarine" => []}
   end
 
-  def place_ship
-    @ships.each {|ship|
-      place_random(ship)
-    }
+  def place_ships
+    @ships.each { |ship| generate_random_coordinates(ship) }
   end
   
-  def place_random ship
-      row_or_column = [@board.valid_rows, @board.valid_rows.transpose][rand(0..1)][rand(0..3)]
-      x = ship.length - 1
-      if !@ships_coordinates[ship.name]
-        @ships_coordinates[ship.name] = []
-      end
-      first_coordinate = row_or_column[rand(0..3)]
-      first_index = row_or_column.index(first_coordinate)
-      @ships_coordinates[ship.name] << first_coordinate
-
-      x.times do
-        @ships_coordinates[ship.name] << row_or_column[first_index += 1]
-        # place_random if @ships_coordinates[ship.name].include?()
-      end
-      valid_placement = @board.valid_placement?(ship, @ships_coordinates[ship.name])
-      if valid_placement
-        @board.place(ship, @ships_coordinates[ship.name]) 
-      else
-        @ships_coordinates = {}
-        place_ship
-      end
+  def generate_random_coordinates ship
+    row_or_column = [@board.valid_rows, @board.valid_rows.transpose][rand(0..1)][rand(0..3)]
+    first_coordinate = row_or_column[rand(0..3)]
+    first_index = row_or_column.index(first_coordinate)
+    @ships_coordinates[ship.name] << first_coordinate
+    remaining_cells = ship.length - 1
+    remaining_cells.times {|_| @ships_coordinates[ship.name] << row_or_column[first_index += 1] }
+    valid_placement = @board.valid_placement?(ship, @ships_coordinates[ship.name])
+    valid_placement ? @board.place(ship, @ships_coordinates[ship.name]) : 
+    (@ships_coordinates[ship.name] = []; generate_random_coordinates(ship))
   end
 end
