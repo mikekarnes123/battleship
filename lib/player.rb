2196a24d7407adcc914bf356be24c90142fb51e1
Paul Kim <pykim0591@gmail.com>
2019-01-31 06:59:07 -0700

    Switch if else to case statement

--- lib/player.rb
+++ lib/player.rb
@@ -22,12 +22,11 @@ class Player
   def place_ship(ship)
     coordinates = place_ship_message(ship)
     valid_coordinates = coordinates.map { |coordinate| @board.valid_coordinate?(coordinate) }
-    if !valid_coordinates.all?
-      error_message(2)
-      place_ship(ship)
-    elsif !@board.valid_placement?(ship, coordinates)
-      error_message(3) 
-      place_ship(ship)
+    case 
+    when !valid_coordinates.all?
+      error_message(2); place_ship(ship)
+    when !@board.valid_placement?(ship, coordinates)
+      error_message(3); place_ship(ship)
     else
       @board.place(ship, coordinates)
     end
